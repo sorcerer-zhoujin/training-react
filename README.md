319edb115ecea2ef0ff4b9c66c5ae1dbb09d1a35
Tomoya Okamoto <49743559+sorcerer-okamoto@users.noreply.github.com>
2023-04-28 18:33:23 +0900

    Okamoto/fix (#1)
    * 環境変数を追加して、4000ポートで起動するようにした。expressとポート番号が被らないようにするため。
    
    * APIアクセスボタンを追加した。
    
    * useEffectwo
    
    ---------
    
    Co-authored-by: wym <wym@sorcerer.co.jp>

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
-#　起動
+# 前準備
+.env.exampleファイルを.envにコピーする
+```shell
+cp .env.example .env
+```
+
+# 起動
 
 1.  `npm install`
 2.  `npm start`
+
--- src/components/PlayerStatus.tsx
+++ src/components/PlayerStatus.tsx
@@ -1,31 +1,39 @@
 import axios from "axios";
-import { useEffect } from "react";
+import { useState } from "react";
 import { Player } from "../types/player";
 
 const PlayerStatus = () => {
   const dummy: Player[] = [{ id: 1, name: "player1", hp: 100, mp: 100, money: 100 }];
+  const [players, setPlayers] = useState<Player[]>([]);
 
-  useEffect(() => {
-    // fetch player by player id from backend
-    const fetchData = async () => {
-      const playerId = localStorage.getItem("playerId");
-      try {
-        //自分のapiサーバーにリクエストを送る
-        const res = await axios.get(`http://localhost:3001/players/${playerId}`);
-        const data = await res.data;
-        console.log(data);
-        //TODO 取得したデータをstateに保存
-      } catch (e) {
-        console.log(e);
-      }
-    };
+  const fetchData = async () => {
+    const playerId = localStorage.getItem("playerId");
+    try {
+      //自分のapiサーバーにリクエストを送る
+      const res = await axios.get(`http://localhost:3001/players/${playerId}`);
+      const data = await res.data;
+      console.log(data);
+      return data;
+      //TODO 取得したデータをstateに保存
+    } catch (e) {
+      console.log(e);
+      return null;
+    }
+  };
 
-    fetchData();
-  }, []);
+  const getAPI = async () => {
+    const result = await fetchData();
+    if (result == null) {
+      setPlayers(dummy);
+    } else {
+      setPlayers(result);
+    }
+  };
 
   return (
     <div>
       <h2 style={{ textAlign: "center" }}>PlayerStatus</h2>
+      <button onClick={getAPI}>APIアクセス</button>
       <table>
         <thead>
           <tr>
@@ -38,7 +46,7 @@ const PlayerStatus = () => {
         </thead>
         <tbody>
           {/* TODO 取得したデータ表示 */}
-          {dummy.map((d) => (
+          {players.map((d) => (
             <tr key={d.id}>
               <td>{d.id}</td>
               <td>{d.name}</td>
